94c0e810628dc5804cbc1015c6d2776d466ff845
DemiPisani <145508667+DemiPisani@users.noreply.github.com>
2023-10-31 18:15:07 -0400

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,24 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id);
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find((book) => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+let booksReturned = books.filter((book) => book.borrows.every((borrow) => borrow.returned === true ));
+  let booksUnreturned = books.filter((book) => book.borrows.some((borrow) => borrow.returned === false));
+  let result = [[...booksUnreturned], [...booksReturned]];
+  return result;
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  return book.borrows.map((borrow) => {
+    let account = accounts.find((account) => account.id === borrow.id);
+    return { ...borrow, ...account}; 
+  }).slice(0,10);
+}
 
 module.exports = {
   findAuthorById,
